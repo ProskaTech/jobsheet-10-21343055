c9a8156b7476acdad3be8fd543de35731d4703f2
21343055 <112170659+21343055@users.noreply.github.com>
2022-11-02 18:26:02 +0700

    Add files via upload

---
+++ Prak 10/src/Latihan/Mahasiswa.java
@@ -0,0 +1,11 @@
+/*
+ * Nama : Muhamad Fathur Rahman
+ * Nim  : 21343055
+ * Prodi: Teknik Informatika
+ */
+package Latihan;
+
+public class Mahasiswa{
+    String nama, jur, univ;
+    int nim;
+}
